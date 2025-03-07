8c7bc2f53d0571c205c5192dc489b3327385aae0
default <yunus.emre.guzel@mths.ca>
2025-03-07 15:02:42 +0000

    added the perimert and area foumals

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@
 // Created on: March 7  2025
 // This file contains the JS functions for index.html
 
-function Helloworld() {
-  document.getElementById("answer").innerHTML = "Hello,world!";
-}
+// function Helloworld() {
+//   document.getElementById("answer").innerHTML = "Hello,world!";
+// }
