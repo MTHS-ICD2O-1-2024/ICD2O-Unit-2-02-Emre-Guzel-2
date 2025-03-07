35604e8b1ce4f37043a8e16b159d53279df8c7ad
default <yunus.emre.guzel@mths.ca>
2025-03-07 17:04:17 +0000

    finshed the JS

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 // Created on: March 7  2025
 // This file contains the JS functions for index.html
 
-// function Helloworld() {
-//   document.getElementById("answer").innerHTML = "Hello,world!";
-// }
+function calculateBtn() {
+  document.getElementById("perimeter").innerHTML = "<p> p = 2(5 + 3) =" + 2*(5+3) + "</p>"
+  document.getElementById("area").innerHTML = "<p> p = 5*3 =" + 5*3 + "</p>"
+
+}
