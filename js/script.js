9ab25991b5263741546cd21f9c1a541baa362895
default <yunus.emre.guzel@mths.ca>
2025-03-07 14:38:48 +0000

    added my own templete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: March 7  2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function Helloworld() {
+  document.getElementById("answer").innerHTML = "Hello,world!";
 }
